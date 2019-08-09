17e20d2a18ffa8f64bef4684d4dc3005c0ea1706
Sidharth <sidnayyar1@gmail.com>
2019-08-09 18:05:13 -0400

    [Sidharth]created mission detail class and sent the object t show the details of mission on mission details. Also Added Logout Button

--- app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/AdapterClass.java
+++ app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/AdapterClass.java
@@ -1,6 +1,7 @@
 package com.LambtonCollege.c0752828_mad3125_midterm;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -38,6 +40,16 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder
         Glide.with(context).load(missionModel.getImage()).into(holder.image1);
         holder.name.setText(missionModel.getName());
         holder.launchYear.setText(missionModel.getLaunchYear());
+        holder.CardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i3 = new Intent(context,MissionDetails.class);
+                i3.putExtra("image",missionModel.getImage());
+                i3.putExtra("name",missionModel.getName());
+                context.startActivity(i3);
+            }
+        });
+
     }
 
     @Override
@@ -46,8 +58,8 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
-
-    ImageView image1;
+        CardView CardView;
+        ImageView image1;
     TextView name;
     TextView launchYear;
         public MyViewHolder(@NonNull View itemView) {
@@ -55,6 +67,8 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder
             image1 = (ImageView)itemView.findViewById(R.id.missileImage);
             name = (TextView)itemView.findViewById(R.id.missionName);
             launchYear = (TextView)itemView.findViewById(R.id.launchYear);
+        CardView = (CardView)itemView.findViewById(R.id.myCardview);
+
         }
     }
 }
--- app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/MissionDetails.java
+++ app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/MissionDetails.java
@@ -1,14 +1,34 @@
 package com.LambtonCollege.c0752828_mad3125_midterm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MissionDetails extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class MissionDetails extends AppCompatActivity {
+    TextView txtName;
+    ImageView missionImage;
+
+String image,name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission_details);
+
+        txtName = (TextView)findViewById(R.id.missionName);
+        missionImage = (ImageView)findViewById(R.id.missionIcon);
+
+        Intent i4 = getIntent();
+       image =  i4.getStringExtra("image");
+       name =  i4.getStringExtra("name");
+        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_SHORT).show();
+        //txtName.setText(name);
+        Glide.with(this).load(image).into(missionImage);
     }
+
 }
