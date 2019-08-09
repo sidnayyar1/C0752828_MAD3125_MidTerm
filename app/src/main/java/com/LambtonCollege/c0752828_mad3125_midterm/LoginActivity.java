3b4f44e220be1387319da5deefaa9cd44e20021f
Sidharth <sidnayyar1@gmail.com>
2019-08-09 03:10:50 -0400

    [Sidharth]Created Login Class With validation

--- app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/LoginActivity.java
+++ app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/LoginActivity.java
@@ -2,15 +2,44 @@ package com.LambtonCollege.c0752828_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edtEmail;
+    EditText edtPassword;
+    Button btnLogin;
+    UserData u1 = new UserData("Sidnayyar","sid123");
+    UserData userData[] = {u1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
+        edtPassword = (EditText)findViewById(R.id.edtPassword);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = edtEmail.getText().toString();
+                String Password = edtPassword.getText().toString();
+                for (int i = 0;i<userData.length;i++){
+                    if (Email.equals(userData[i].getEmail()) && Password.equals(userData[i].getPassword())){
+                        Intent toLogin = new Intent(LoginActivity.this,HomeActivity.class);
+                        startActivity(toLogin);
+                    }else {
+                        Toast.makeText(LoginActivity.this,"Incorrect email or password",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
     }
 
