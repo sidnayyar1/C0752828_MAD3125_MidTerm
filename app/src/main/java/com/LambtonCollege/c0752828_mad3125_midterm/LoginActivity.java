754b1359ebb51b196b5d8bce15bd101215ee0ca0
Sidharth <sidnayyar1@gmail.com>
2019-08-09 03:19:10 -0400

    [Sidharth] Shared Preferences applied.

--- app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/LoginActivity.java
+++ app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/LoginActivity.java
@@ -3,6 +3,7 @@ package com.LambtonCollege.c0752828_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     UserData u1 = new UserData("Sidnayyar","sid123");
     UserData userData[] = {u1};
+SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,8 @@ public class LoginActivity extends AppCompatActivity {
         edtEmail = (EditText)findViewById(R.id.edtEmail);
         edtPassword = (EditText)findViewById(R.id.edtPassword);
         btnLogin = (Button)findViewById(R.id.btnLogin);
-
+        sharedPreferences = getSharedPreferences("userDetails",MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,6 +35,9 @@ public class LoginActivity extends AppCompatActivity {
                 String Password = edtPassword.getText().toString();
                 for (int i = 0;i<userData.length;i++){
                     if (Email.equals(userData[i].getEmail()) && Password.equals(userData[i].getPassword())){
+                       editor.putString("email",Email);
+                       editor.putString("password",Password);
+                       editor.apply();
                         Intent toLogin = new Intent(LoginActivity.this,HomeActivity.class);
                         startActivity(toLogin);
                     }else {
