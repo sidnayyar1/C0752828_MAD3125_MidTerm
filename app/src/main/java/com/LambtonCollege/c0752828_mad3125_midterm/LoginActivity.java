00c4f40779ea492bbd82515964665be224e9ee77
Sidharth <sidnayyar1@gmail.com>
2019-08-09 01:45:39 -0400

    [Sidharth] changed code of Splash Screen

--- app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/LoginActivity.java
+++ app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/LoginActivity.java
@@ -9,7 +9,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.LoginActivity);
+        setContentView(R.layout.activity_login);
 
 
     }
