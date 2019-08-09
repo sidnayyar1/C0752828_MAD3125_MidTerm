251dc600f6b416bf28bb2ccbd13db222eb343ae2
Sidharth <sidnayyar1@gmail.com>
2019-08-09 17:20:21 -0400

    [Sidharth]Added mission detials Activity

---
+++ app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/MissionDetails.java
@@ -0,0 +1,14 @@
+package com.LambtonCollege.c0752828_mad3125_midterm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MissionDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_details);
+    }
+}
