44570c0749f6a5699161ac5118443d1cccf0ece7
Sidharth <sidnayyar1@gmail.com>
2019-08-09 00:48:18 -0400

    [Sidharth Nayyar] initial Commit

---
+++ app/src/main/java/com/LambtonCollege/c0752828_mad3125_midterm/MainActivity.java
@@ -0,0 +1,16 @@
+package com.LambtonCollege.c0752828_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
